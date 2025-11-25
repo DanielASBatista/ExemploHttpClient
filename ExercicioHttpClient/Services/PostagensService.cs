70b9200a85484bbf91e75c433175ae227a6eaee7
DanielASBatista <daniel_alves.santos@hotmail.com>
2025-11-24 22:32:27 -0300

    Adição: Services.

---
+++ ExercicioHttpClient/Services/PostagensService.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicioHttpClient.Models;
+using Newtonsoft.Json; 
+
+namespace ExercicioHttpClient.Services
+{
+    public class PostagensService
+    {
+        async public Task <List<Postagem>> GetPostagens()
+        {
+            //Pedir uma informação para a A
+            //PI
+            //Pegar o dado, que veio da API
+            //Formatar o dado para que ele seja um objeto
+
+            List<Postagem> postagens = new List<Postagem>();
+            string URI = "https://jsonplaceholder.typicode.com/posts";
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage responseMessage = await httpClient.GetAsync(URI);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //É necessário Serializar e Desserializar a informação
+                string conteudo = await responseMessage.Content.ReadAsStringAsync();
+                postagens = JsonConvert.DeserializeObject<List<Postagem>>(conteudo);
+                
+            }
+            return (postagens);
+        }
+    }
+}
--- ExercicioHttpClient/ViewModel/PostagensViewModel.cs
+++ ExercicioHttpClient/ViewModel/PostagensViewModel.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ExercicioHttpClient.Models;
+using ExercicioHttpClient.Services;
 
-namespace ExercicioHttpClient.ViewModel
+namespace ExercicioHttpClient.ViewModels
 {
-    internal class PostagensViewModel
+    public class PostagensViewModel : ObservableObject
     {
+        [ObservableProperty]
+        public int id;
+        [ObservableProperty]
+        public int userid;
+        [ObservableProperty]
+        string title;
+        [ObservableProperty]
+        string body;
+    
+        public async void CarregarPostagens()
+        {
+            List<Postagem> Listar = new List<Postagem>();
+            Listar = await new PostagensService().GetPostagens();
+        }
+    
     }
 }
